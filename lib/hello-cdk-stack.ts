473a82f0b9a52448049a603846f031da7ed54bcd
ThuF <jordan.p.pavlov@gmail.com>
2022-11-18 14:17:53 +0200

    CDK - Initial

---
+++ lib/hello-cdk-stack.ts
@@ -0,0 +1,24 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { aws_s3 as s3 } from 'aws-cdk-lib';
+
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class HelloCdkStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    new s3.Bucket(this, "MyBucket", {
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true
+    });
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'HelloCdkQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
